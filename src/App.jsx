2afe48abfb5a21ebb753bf398655cb28c660355d
NeilB1ju <95778971+NeilB1ju@users.noreply.github.com>
2023-09-27 23:49:10 +0530

    Added components and global state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Header from './components/Header';
+import Balance from './components/Balance';
+import './App.css';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </>
+  );
 }
 
 export default App;
